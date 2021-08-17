32f03d9f4a2b36d376d253cb1668221b660f4c57
Siemen Blok <siemenblok@hotmail.com>
2021-08-17 22:17:05 +0200

    update, comments, pug, permissions

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const axios = require('axios')
+const pug = require('pug')
+const Path = require('path')
 const { PrismaClient } = require('@prisma/client')
 
 const ProductInsert = require('./product.template')
+const selectPermissions = require('./selectPermissions')
 const qiriBaseURL = process.env.QIRI_URL
 
 const prisma = new PrismaClient()
@@ -13,13 +16,17 @@ const port = 9000
 
 app.use(express.json())
 
+/*
+* This is just to seed the database with all the products that can be found at the endpoint
+* provided in the case for Sellvation. The SKU's should be provided as an array in the payload (body)
+* to start the procces. If a retrieval or insertion failed it will be returned in the response. 
+*/
+
 app.post('/seed/skus', async (req, res) => {
     const skus = req.body?.skus
-    if(!(skus.length > 0) || !(typeof skus === 'object')) {
-        res.status(400).json({message: `No SKU's provided, please provide an array of SKU's`})
-        return
-    }
-
+    // At the moment I chose to let the call fail when no array was provided
+    if(!skus || skus.length === 0 || !Array.isArray(skus)) return res.status(400).json({message: `No SKU's provided, please provide an array of SKU's`}) 
+    
     let failedToRetrieve = []
     let failedToInsert = []
 
@@ -45,6 +52,7 @@ app.post('/seed/skus', async (req, res) => {
         return
     }
 
+    // Return only the arrays if there are items in them. 
     return res.status(200).json({
         message: 'Inserted the products into the DB', 
         ...failedToInsert.length > 0 && {failedToInsert},
@@ -55,10 +63,15 @@ app.post('/seed/skus', async (req, res) => {
 
 app.get('/product/:sku', async (req, res) => {
     const sku = req.params?.sku
+
+    // If no sku was provided return an object indicating this. 
+    if(!sku) return res.status(400).json({message: 'No SKU was provided, please provide a sku to retrieve the product.'})
+    
     try{
         // Get the products from Qiri, if an SKU is entered that is not in Qiri product should default to undefined. 
         const response = (await axios.get(`${qiriBaseURL}&sku=${sku}`))?.data
 
+        // If there is a response, format the object to the specified data points described in the case
         if(response) {
             const {product} = response
             const {name, pricing} = product
@@ -74,43 +87,78 @@ app.get('/product/:sku', async (req, res) => {
 })
 
 app.get('/table', async (req, res) => {
+    const bodySKUs = req.body?.skus
 
+    // If no array was provided take the default array as described in the case. 
+    const seedSKUs = ['103850','103580','103613', '103664', '103850', '105146', '105149', '105167']
+    const skus = bodySKUs ? bodySKUs : seedSKUs
+
+    // Get the path to the pug template to render the HTML-table. 
+    const template = Path.resolve(__dirname, `./productTable.template.pug`)
+    let products = []
+
+    for (let sku of skus){
+        // Retrieve the product with the provided sku. 
+        const product = (await axios.get(`${qiriBaseURL}&sku=${sku}`).catch(err => {
+            console.error(err.response.data)
+            failedToRetrieve = [...failedToRetrieve, {sku, reason: err.response.data.message}]
+        }))?.data
+        
+        /* 
+        * If product is undefined the product should not be rendered in the HTML-table. 
+        * So we can skip this product and continue with the rest. 
+        */ 
+        if(!product) continue
+    
+        products = [...products, new ProductInsert(product)]
+    }
+
+    // Create an HTML-string based on the template
+    const file = pug.renderFile(template, {products})
+
+    /* 
+    * If the HTML-string was created send the string to the browser. 
+    * Else return a 404 error telling that the products could nog be found
+    */ 
+
+    if(file) return res.send(file)
+    else return res.status(404).json({message: 'Could not find products'})
 })
 
 app.get('/pgTable', async (req, res) => {
-    const table = await prisma.products.findMany({
-        select: {
-            id: false,
-            sku: true, 
-            name: true, 
-            product_id: true, 
-            unit_type: true,
-            currency: true,
-            list_price: true,
-            vat: true, 
-            selling_price: true, 
-            unit_quantity: true, 
-            unavailable: true, 
-            available_in_webshop: true,
-            is_organic: true, 
-            is_ecological: true, 
-            is_private_label: true
-        }
+    // Get the path to the pug template to render the HTML-table. 
+    const template = Path.resolve(__dirname, `./productTable.template.pug`)
+    
+    // Retrieve the products from the database. 
+    const products = await prisma.products.findMany({
+        select: selectPermissions['enitireTable']
     })
-    if(table){
-        return res.status(200).json(table)
-    }else{
-        return res.status(500)
-    }
+
+    /* 
+    * Create an HTML-string based on the template if the products were found
+    * Unlike the endpoint /table there does not need to be a check on if a HTML-string
+    * was created since we can asume the database was filled with products and there 
+    * should be enough info to create a table
+    */
+
+    if(products) return res.send(pug.renderFile(template, {products}))
+    else return res.status(404).json({message: 'Could not find products in the table.'})
 })
 
 app.get('/pgTable/:sku', async (req, res) => {
-    const sku = req.params.sku
+    const sku = req.params?.sku
+
+    // If no SKU was provided a message should be returned the request was malformed and a SKU should be provided
+    if(!sku) return res.status(400).json({message: 'Please provide a SKU'})
+
+    // Select the product matching the SKU
     const product = await prisma.products.findFirst({
         where: {
             sku: sku
         }
     })
+
+    // If  a product was found, return it, otherwise return 404 with a message the product could not be found. 
     if(product) return res.status(200).json({data: product})
     else return res.status(404).json({message: `Could not find a product with sku ${sku}`})
 })
---
+++ backend/selectPermissions.js
@@ -0,0 +1,19 @@
+module.exports = {
+    enitireTable: {
+        id: false,
+        sku: true, 
+        name: true, 
+        product_id: true, 
+        unit_type: true,
+        currency: true,
+        list_price: true,
+        vat: true, 
+        selling_price: true, 
+        unit_quantity: true, 
+        unavailable: true, 
+        available_in_webshop: true,
+        is_organic: true, 
+        is_ecological: true, 
+        is_private_label: true
+    }
+}
